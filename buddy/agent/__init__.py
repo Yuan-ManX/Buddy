eb2cf31af411bcbbc69a0c8d5676de15c3b4ca3f
Yuan-Man <ym1076302261@163.com>
2026-06-21 19:04:17 +0800

    fix: update Buddy

--- buddy/agent/__init__.py
+++ buddy/agent/__init__.py
@@ -76,6 +76,63 @@ from agent.resource import ResourceManager, ResourceQuota, ResourceType, QuotaPe
 from agent.agent_intelligence import AgentIntelligence, IntelligenceConfig, IntelligenceMode, ReasoningStrategy, TaskComplexity as IntelTaskComplexity, ThinkingStep, ReasoningTrace, Experience as IntelExperience, ToolRelevance
 from agent.agent_core import AgentCore, AgentCoreConfig, AgentState, ExecutionContext, AgentCapability, ExecutionStep as CoreExecutionStep, ExecutionTrace as CoreExecutionTrace, AgentInsight as CoreAgentInsight, ProactiveSignal as CoreProactiveSignal
 from agent.agent_synthesis import AgentSynthesis, agent_synthesis, SynthesisMode, InsightType, SynthesisInsight, AgentContribution
+from agent.agent_persona import PersonaRegistry, PersonaProfile, PersonaTrait, InteractionStyle, DecisionStyle, persona_registry
+from agent.agent_governance import GovernanceEngine, PolicyRule, PolicyLevel, PolicyAction, PolicyCategory, ApprovalRequest, BudgetTracker, governance_engine
+from agent.workspace_manager import WorkSpaceManager, WorkSpace, WorkSpaceConfig, WorkSpaceSnapshot, workspace_manager
+from agent.smart_router import SmartRouter, TaskComplexity, ModelTier, ModelConfig, RoutingDecision, smart_router
+from agent.identity_core import IdentityCore, IdentityRegistry, MemoryLayer, TraitCategory, IdentityTrait, EpisodicEntry, SemanticNode, ProceduralPattern, identity_registry
+from agent.agent_mesh import AgentMesh, MeshNode, MeshNodeConfig, MeshNodeState, MeshTask, TaskPriority, DelegationStrategy, agent_mesh
+from agent.learning_loop import LearningLoop, ObservationEngine, ExtractionEngine, CompoundingEngine, EvolutionEngine, NudgeEngine, ObservationType, SkillSource, NudgePriority, NudgeCategory, learning_loop
+from agent.kgraph import KnowledgeGraph, EntityStore, RelationStore, InferenceEngine, EntityType, RelationType, Entity, Relation, knowledge_graph
+from agent.experience_engine import ExperienceEngine, ExperienceRecorder, ExperienceReplayBuffer, AgentTrajectoryCompressor, ExperienceEvolver, ExperienceAnalytics, ExperienceKind, ExperienceResult, EmotionalValence, experience_engine
+from agent.collab_space import CollaborationSpace, CollaborationRoom, CollaborationSession, ArtifactBoard, ConsensusEngine, CollaborationAnalytics, RoomType, RoomState, SessionStatus, MessageRole, ArtifactType, collab_space
+from agent.context_engine import ContextEngine, ContextAssembler, ContextCompressor, ContextInjector, ContextWindow, ContextAnalytics, ContextSource, ContextPriority, CompressionStrategy, context_engine
+from agent.automation_core import AutomationCore, AutomationRegistry, CronScheduler, AutomationRunner, EventWatcher, AutomationAnalytics, AutomationType, TriggerType, AutomationLifecycle, automation_core
+from agent.skill_fabric import SkillFabric, SkillForge, SkillBundleManager, SkillMarket, SkillComposer, SkillAnalytics, SkillType, SkillLifecycleStatus, PricingModel, skill_fabric
+from agent.user_model import UserModelEngine, user_model_engine, TraitDimension, ConfidenceLevel, UserTrait, UserProfile, InteractionSnapshot
+from agent.evolving_skills import SelfEvolvingSkillRegistry, EvolvingSkill, SkillVariant, SkillEvolutionStage, VariantStrategy, ExecutionRecord, evolving_skills
+from agent.agent_protocol import ProtocolEngine, MessageRouter, EventBus, ComponentRegistry, StateSynchronizer, ProtocolMessage, MessageType, MessagePriority, ComponentState, ComponentInfo, protocol_engine
+from agent.subagent import SubAgentMesh, Workstream, WorkstreamStatus, WorkstreamManager, get_subagent_mesh
+from agent.sandbox import SandboxEngine, SandboxSession, SandboxConfig, SandboxPolicy, get_sandbox_engine
+from agent.streaming import StreamingEngine, StreamSession, StreamEvent, StreamEventType, get_streaming_engine
+from agent.tool_executor import ToolExecutor, ToolRegistry, ToolDefinition, ToolCategory, ToolRisk, ToolExecution, get_tool_executor
+from agent.browser_agent import BrowserAgent, BrowserSession, BrowserAction, BrowserState, get_browser_agent
+from agent.terminal_agent import TerminalAgent, TerminalSession, TerminalConfig, TerminalMode, TerminalResult, get_terminal_agent
+from agent.plan_executor import PlanExecutor, ExecutionPlan, PlanStep, PlanStatus, StepType, PlanGenerator, get_plan_executor
+from agent.model_orchestrator import ModelOrchestrator, ModelRouter, ModelConfig, ModelProvider, ModelCapability, ModelRequest, ModelResponse, get_model_orchestrator
+from agent.deployment import DeploymentPipeline, Deployment, DeploymentConfig, DeploymentStatus, DeploymentTarget, get_deployment_pipeline
+from agent.telemetry import TelemetryEngine, MetricRegistry, TraceCollector, MetricType, TraceLevel, TraceSpan, get_telemetry_engine
+from agent.mcp_connector import MCPConnector, MCPServerConnection, MCPServerConfig, MCPTool, MCPResource, MCPConnectionState, MCPTransport, get_mcp_connector
+from agent.integration_hub import IntegrationHub, Integration, IntegrationConfig, IntegrationType, AuthMethod, get_integration_hub
+from agent.product_composer import ProductComposer, ProductDefinition, ProductComponent, ComponentType, ProductStatus, get_product_composer
+from agent.agent_orchestrator import AgentOrchestrator, SubAgentProfile, Workstream, WorkstreamType, Squad, AgentLifecycle, agent_orchestrator
+from agent.dream_mode import DreamMode, DreamSession, DreamPhase, MemoryEntry, ProactiveTask, dream_mode
+from agent.white_memory import WhiteMemory, WhiteMemoryEntry, MemoryCategory, MemoryLifecycleStage, MemoryProvenance, MemoryAuditEntry, white_memory
+from agent.agent_reflection import AgentReflectionEngine, ReflectionRecord, ReflectionDimension, ErrorCategory, ReflectionStatus, QualityScore, DetectedError, reflection_engine
+from agent.agent_intent import AgentIntentEngine, IntentResult, IntentCategory, IntentComplexity, IntentUrgency, IntentEntity, IntentSession, intent_engine
+from agent.agent_fleet import AgentFleetManager, FleetAgent, FleetAgentStatus, FleetHealth, FleetLoadReport, FleetHealthReport, fleet_manager
+from agent.event_pipeline import EventPipeline, PipelineEvent, EventSource, EventPriority, EventSubscription, DeadLetterEntry, event_pipeline
+from agent.knowledge_network import KnowledgeNetwork, KnowledgeEntry, KnowledgeType, KnowledgeStatus, VerificationLevel, KnowledgeTopic, knowledge_network
+from agent.agent_reasoning import AgentReasoningEngine, ReasoningTrace, ReasoningStep, ReasoningStrategy, Hypothesis, reasoning_engine
+from agent.agent_tool_composer import AgentToolComposer, ToolPipeline, PipelineStage, ToolNode, PipelineResult, ExecutionMode as ToolExecutionMode, tool_composer
+from agent.agent_context_manager import AgentContextManager, ContextItem, ContextType, ContextPriority, ContextSnapshot, context_manager
+from agent.model_proxy import ModelProxyLayer, ModelProfile, ProviderType, ModelCapability, ProxyStrategy, ProxyRequest, ProxyResponse, model_proxy
+from agent.skill_compiler import SkillCompiler, SkillDefinition, SkillStatus, SkillLanguage, SkillParameter, CompilationResult, skill_compiler
+from agent.unified_runtime import UnifiedAgentRuntime, RuntimeSession, RuntimeMode, RuntimePhase, unified_runtime
+from agent.agent_deep_reasoning import DeepReasoningEngine, DeepReasoningResult, ThoughtNode, ReasoningBranch, BranchStatus, VoteStrategy, deep_reasoning
+from agent.agent_self_improve import SelfImprovementEngine, SynthesizedSkill, AgentNudge, NudgeType, NudgePriority, SkillOrigin, SkillLifecycle, ImprovementMetrics, self_improvement
+from agent.agent_session import AgentSessionManager, CollaborativeSession, SessionMessage, SessionArtifact, SessionParticipant, SessionRole, SessionState, MessageRole, agent_session_manager
+from agent.agent_unified_brain import UnifiedBrain, BrainContext, BrainPerception, BrainCognition, BrainAction, BrainReflection, BrainCycleResult, BrainMode, PerceptionType, CognitivePhase, unified_brain
+from agent.agent_platform_core import PlatformCore, PlatformConfig, RuntimeInstance, RuntimeState, SandboxEnvironment, SandboxType, HealthReport, HealthStatus, PlatformAlert, AlertSeverity, ContextSyncEvent, platform_core
+from agent.agent_flow import AgentFlow, FlowPhase, CorrectionStrategy, OutputFormat, OutputSchema, ReasoningPath, ToolCallRecord, SelfCorrection, FlowResult, agent_flow
+from agent.agent_mcp import MCPRegistry, MCPToolDefinition, MCPToolCategory, MCPServerConfig, MCPServerType, MCPToolResult, MCPResource, MCPPrompt, MCPToolExecutor, mcp_registry, mcp_executor
+from agent.agent_autonomous_loop import AutonomousLoopEngine, AutonomousGoal, GoalStatus, GoalStep, StepStatus, autonomous_loop
+from agent.agent_permission import PermissionManager, PermissionPolicy, PermissionLevel, ApprovalStatus, PolicyScope, ApprovalRequest, ExecutionAudit, permission_manager
+from agent.agent_profile import ProfileManager, AgentProfile, CommunicationStyle, ExpertiseLevel, InteractionMode, PersonalityTrait, KnowledgeDomain, BehavioralRule, profile_manager
+from agent.agent_browser import BrowserAutomation, BrowserSession, BrowserAction, SelectorType, BrowserElement, BrowserActionStep, BrowserActionResult, browser_automation
+from agent.agent_system_tools import SystemToolManager, CommandRisk, FileOperation, CommandResult, FileOperationResult, ClipboardContent, system_tools
+from agent.platform_pipeline import PipelineEngine, Pipeline, PipelineStage, StageStatus, PipelineType, PipelineStatus, PipelineCheckpoint, pipeline_engine
+from agent.platform_gateway import PlatformGateway, ProviderCatalog, ProviderConfig, ProviderType, ProviderStatus, RoutingStrategy, ProviderCapability, RoutingRule, GatewayRequest, GatewayResponse, platform_gateway
 
 __all__ = [
     "AgentEngine",
@@ -114,4 +171,62 @@ __all__ = [
     "AgentCore", "AgentCoreConfig", "AgentState", "ExecutionContext", "AgentCapability", "CoreExecutionStep", "CoreExecutionTrace", "CoreAgentInsight", "CoreProactiveSignal",
     "AgentSynthesis", "agent_synthesis", "SynthesisMode", "InsightType", "SynthesisInsight", "AgentContribution",
     "orchestrator", "skills_registry",
+    # New platform modules
+    "PersonaRegistry", "PersonaProfile", "PersonaTrait", "InteractionStyle", "DecisionStyle", "persona_registry",
+    "GovernanceEngine", "PolicyRule", "PolicyLevel", "PolicyAction", "PolicyCategory", "ApprovalRequest", "BudgetTracker", "governance_engine",
+    "WorkSpaceManager", "WorkSpace", "WorkSpaceConfig", "WorkSpaceSnapshot", "workspace_manager",
+    "SmartRouter", "TaskComplexity", "ModelTier", "ModelConfig", "RoutingDecision", "smart_router",
+    "IdentityCore", "IdentityRegistry", "MemoryLayer", "TraitCategory", "IdentityTrait", "EpisodicEntry", "SemanticNode", "ProceduralPattern", "identity_registry",
+    "AgentMesh", "MeshNode", "MeshNodeConfig", "MeshNodeState", "MeshTask", "TaskPriority", "DelegationStrategy", "agent_mesh",
+    "LearningLoop", "ObservationEngine", "ExtractionEngine", "CompoundingEngine", "EvolutionEngine", "NudgeEngine", "ObservationType", "SkillSource", "NudgePriority", "NudgeCategory", "learning_loop",
+    "KnowledgeGraph", "EntityStore", "RelationStore", "InferenceEngine", "EntityType", "RelationType", "Entity", "Relation", "knowledge_graph",
+    "ExperienceEngine", "ExperienceRecorder", "ExperienceReplayBuffer", "AgentTrajectoryCompressor", "ExperienceEvolver", "ExperienceAnalytics", "ExperienceKind", "ExperienceResult", "EmotionalValence", "experience_engine",
+    "CollaborationSpace", "CollaborationRoom", "CollaborationSession", "ArtifactBoard", "ConsensusEngine", "CollaborationAnalytics", "RoomType", "RoomState", "SessionStatus", "MessageRole", "ArtifactType", "collab_space",
+    "ContextEngine", "ContextAssembler", "ContextCompressor", "ContextInjector", "ContextWindow", "ContextAnalytics", "ContextSource", "ContextPriority", "CompressionStrategy", "context_engine",
+    "AutomationCore", "AutomationRegistry", "CronScheduler", "AutomationRunner", "EventWatcher", "AutomationAnalytics", "AutomationType", "TriggerType", "AutomationLifecycle", "automation_core",
+    "SkillFabric", "SkillForge", "SkillBundleManager", "SkillMarket", "SkillComposer", "SkillAnalytics", "SkillType", "SkillLifecycleStatus", "PricingModel", "skill_fabric",
+    "UserModelEngine", "user_model_engine", "TraitDimension", "ConfidenceLevel", "UserTrait", "UserProfile", "InteractionSnapshot",
+    "SelfEvolvingSkillRegistry", "EvolvingSkill", "SkillVariant", "SkillEvolutionStage", "VariantStrategy", "ExecutionRecord", "evolving_skills",
+    "ProtocolEngine", "MessageRouter", "EventBus", "ComponentRegistry", "StateSynchronizer", "ProtocolMessage", "MessageType", "MessagePriority", "ComponentState", "ComponentInfo", "protocol_engine",
+    "SubAgentMesh", "Workstream", "WorkstreamStatus", "WorkstreamManager", "get_subagent_mesh",
+    "SandboxEngine", "SandboxSession", "SandboxConfig", "SandboxPolicy", "get_sandbox_engine",
+    "StreamingEngine", "StreamSession", "StreamEvent", "StreamEventType", "get_streaming_engine",
+    "ToolExecutor", "ToolRegistry", "ToolDefinition", "ToolCategory", "ToolRisk", "ToolExecution", "get_tool_executor",
+    "BrowserAgent", "BrowserSession", "BrowserAction", "BrowserState", "get_browser_agent",
+    "TerminalAgent", "TerminalSession", "TerminalConfig", "TerminalMode", "TerminalResult", "get_terminal_agent",
+    "PlanExecutor", "ExecutionPlan", "PlanStep", "PlanStatus", "StepType", "PlanGenerator", "get_plan_executor",
+    "ModelOrchestrator", "ModelRouter", "ModelConfig", "ModelProvider", "ModelCapability", "ModelRequest", "ModelResponse", "get_model_orchestrator",
+    "DeploymentPipeline", "Deployment", "DeploymentConfig", "DeploymentStatus", "DeploymentTarget", "get_deployment_pipeline",
+    "TelemetryEngine", "MetricRegistry", "TraceCollector", "MetricType", "TraceLevel", "TraceSpan", "get_telemetry_engine",
+    "MCPConnector", "MCPServerConnection", "MCPServerConfig", "MCPTool", "MCPResource", "MCPConnectionState", "MCPTransport", "get_mcp_connector",
+    "IntegrationHub", "Integration", "IntegrationConfig", "IntegrationType", "AuthMethod", "get_integration_hub",
+    "ProductComposer", "ProductDefinition", "ProductComponent", "ComponentType", "ProductStatus", "get_product_composer",
+    "AgentOrchestrator", "SubAgentProfile", "Workstream", "WorkstreamType", "Squad", "AgentLifecycle", "agent_orchestrator",
+    "DreamMode", "DreamSession", "DreamPhase", "MemoryEntry", "ProactiveTask", "dream_mode",
+    "WhiteMemory", "WhiteMemoryEntry", "MemoryCategory", "MemoryLifecycleStage", "MemoryProvenance", "MemoryAuditEntry", "white_memory",
+    "AgentReflectionEngine", "ReflectionRecord", "ReflectionDimension", "ErrorCategory", "ReflectionStatus", "QualityScore", "DetectedError", "reflection_engine",
+    "AgentIntentEngine", "IntentResult", "IntentCategory", "IntentComplexity", "IntentUrgency", "IntentEntity", "IntentSession", "intent_engine",
+    "AgentFleetManager", "FleetAgent", "FleetAgentStatus", "FleetHealth", "FleetLoadReport", "FleetHealthReport", "fleet_manager",
+    "EventPipeline", "PipelineEvent", "EventSource", "EventPriority", "EventSubscription", "DeadLetterEntry", "event_pipeline",
+    "KnowledgeNetwork", "KnowledgeEntry", "KnowledgeType", "KnowledgeStatus", "VerificationLevel", "KnowledgeTopic", "knowledge_network",
+    "AgentReasoningEngine", "ReasoningTrace", "ReasoningStep", "ReasoningStrategy", "Hypothesis", "reasoning_engine",
+    "AgentToolComposer", "ToolPipeline", "PipelineStage", "ToolNode", "PipelineResult", "ToolExecutionMode", "tool_composer",
+    "AgentContextManager", "ContextItem", "ContextType", "ContextPriority", "ContextSnapshot", "context_manager",
+    "ModelProxyLayer", "ModelProfile", "ProviderType", "ModelCapability", "ProxyStrategy", "ProxyRequest", "ProxyResponse", "model_proxy",
+    "SkillCompiler", "SkillDefinition", "SkillStatus", "SkillLanguage", "SkillParameter", "CompilationResult", "skill_compiler",
+    "UnifiedAgentRuntime", "RuntimeSession", "RuntimeMode", "RuntimePhase", "unified_runtime",
+    "DeepReasoningEngine", "DeepReasoningResult", "ThoughtNode", "ReasoningBranch", "BranchStatus", "VoteStrategy", "deep_reasoning",
+    "SelfImprovementEngine", "SynthesizedSkill", "AgentNudge", "NudgeType", "NudgePriority", "SkillOrigin", "SkillLifecycle", "ImprovementMetrics", "self_improvement",
+    "AgentSessionManager", "CollaborativeSession", "SessionMessage", "SessionArtifact", "SessionParticipant", "SessionRole", "SessionState", "MessageRole", "agent_session_manager",
+    "UnifiedBrain", "BrainContext", "BrainPerception", "BrainCognition", "BrainAction", "BrainReflection", "BrainCycleResult", "BrainMode", "PerceptionType", "CognitivePhase", "unified_brain",
+    "PlatformCore", "PlatformConfig", "RuntimeInstance", "RuntimeState", "SandboxEnvironment", "SandboxType", "HealthReport", "HealthStatus", "PlatformAlert", "AlertSeverity", "ContextSyncEvent", "platform_core",
+    "AgentFlow", "FlowPhase", "CorrectionStrategy", "OutputFormat", "OutputSchema", "ReasoningPath", "ToolCallRecord", "SelfCorrection", "FlowResult", "agent_flow",
+    "MCPRegistry", "MCPToolDefinition", "MCPToolCategory", "MCPServerConfig", "MCPServerType", "MCPToolResult", "MCPResource", "MCPPrompt", "MCPToolExecutor", "mcp_registry", "mcp_executor",
+    "AutonomousLoopEngine", "AutonomousGoal", "GoalStatus", "GoalStep", "StepStatus", "autonomous_loop",
+    "PermissionManager", "PermissionPolicy", "PermissionLevel", "ApprovalStatus", "PolicyScope", "ApprovalRequest", "ExecutionAudit", "permission_manager",
+    "ProfileManager", "AgentProfile", "CommunicationStyle", "ExpertiseLevel", "InteractionMode", "PersonalityTrait", "KnowledgeDomain", "BehavioralRule", "profile_manager",
+    "BrowserAutomation", "BrowserSession", "BrowserAction", "SelectorType", "BrowserElement", "BrowserActionStep", "BrowserActionResult", "browser_automation",
+    "SystemToolManager", "CommandRisk", "FileOperation", "CommandResult", "FileOperationResult", "ClipboardContent", "system_tools",
+    "PipelineEngine", "Pipeline", "PipelineStage", "StageStatus", "PipelineType", "PipelineStatus", "PipelineCheckpoint", "pipeline_engine",
+    "PlatformGateway", "ProviderCatalog", "ProviderConfig", "ProviderType", "ProviderStatus", "RoutingStrategy", "ProviderCapability", "RoutingRule", "GatewayRequest", "GatewayResponse", "platform_gateway",
 ]
